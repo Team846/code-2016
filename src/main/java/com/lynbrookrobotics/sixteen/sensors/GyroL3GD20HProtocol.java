93d4b9efc8b2b9bd50f2b71c01ee33484bcb9483
Philip Axelrod <philip.axelrod.web@gmail.com>
2016-01-17 00:22:58 -0800

    Fixed memory leak
    In method isFIFOFUll(), replaced creating new byte array every time with
    referencing an already declared, global byte array

--- src/main/java/com/lynbrookrobotics/sixteen/sensors/GyroL3GD20HProtocol.java
+++ src/main/java/com/lynbrookrobotics/sixteen/sensors/GyroL3GD20HProtocol.java
@@ -35,6 +35,9 @@ class GyroL3GD20HProtocol {
     private  byte[] inputFromSlave = new byte[7];
     private  byte[] outputToSlave = new byte[7];
 
+    private  byte[] FIFOInputFromSlave = new byte[2];
+    private  byte[] FIFOOutputToSlave = new byte[2];
+
     private double xFIFO = 0;
     private double xVel = 0;
 
@@ -84,11 +87,11 @@ class GyroL3GD20HProtocol {
 
         if (mode == STREAM_MODE) {
             out[0] = (byte) (L3GD20_REGISTER_CTRL_REG5);
-            out[1] = (byte) 0b01000000;//byte to enable FIFO
+            out[1] = (byte) 0b01000000;//byte to enable FIFO, and limit the FIFO to an amount specified later
             gyro.transaction(out, in, SETTING_BYTES_SENT_RECEIVED);
 
             out[0] = (byte) (L3GD20_REGISTER_FIFO_CTRL);
-            out[1] = (byte) (0b01000000);//byte that sets to stream mode
+            out[1] = (byte) (0b01100000);//byte that sets to stream mode and FIFO threshold (limit) to 16
             gyro.transaction(out, in, SETTING_BYTES_SENT_RECEIVED);
         }
 
@@ -164,11 +167,10 @@ class GyroL3GD20HProtocol {
      * @returns: returns whether the FIFO on the gyro is full or not
      */
     private boolean isFIFOFull() {
-        byte[] outputToSlave = new byte[SETTING_BYTES_SENT_RECEIVED];//from RoboRio to slave (gyro)
-        outputToSlave[0] = setByte(L3GD20_REGISTER_FIFO_CTRL, (byte) 0b10000000); // set bit 0 (READ bit) to 1 (pg. 31)
-        byte[] inputFromSlave = new byte[SETTING_BYTES_SENT_RECEIVED];
+        //byte[] outputToSlave = new byte[SETTING_BYTES_SENT_RECEIVED];//from RoboRio to slave (gyro)
+        FIFOOutputToSlave[0] = setByte(L3GD20_REGISTER_FIFO_CTRL, (byte) 0b10000000); // set bit 0 (READ bit) to 1 (pg. 31)
 
-        gyro.transaction(outputToSlave, inputFromSlave, READING_BYTES_SENT_RECEIVED);//read from FIFO control register
+        gyro.transaction(FIFOOutputToSlave, FIFOInputFromSlave, READING_BYTES_SENT_RECEIVED);//read from FIFO control register
 
 
         if (((inputFromSlave[1] >> 6) & 0b1) == 0b1){//if second bit from the left is 1 then it is full
