a9e65616720916a27a104286d0d608b6d7435d52
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-01-18 15:14:22 -0800

    Adds ConstantBufferSPI, which uses a preallocated ByteBuffer + This will be used to reduce memory usage as described in #2

---
+++ src/main/java/com/lynbrookrobotics/sixteen/sensors/ConstantBufferSPI.java
@@ -0,0 +1,35 @@
+package com.lynbrookrobotics.sixteen.sensors;
+
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.hal.SPIJNI;
+
+import java.nio.ByteBuffer;
+
+public class ConstantBufferSPI extends SPI {
+    private byte port;
+    private byte size;
+
+    private ByteBuffer sendBuffer;
+    private ByteBuffer receiveBuffer;
+
+    /**
+     * Constructor
+     *
+     * @param port the physical SPI port
+     */
+    public ConstantBufferSPI(Port port, int size) {
+        super(port);
+
+        this.size = (byte) size;
+        this.port = (byte) port.getValue();
+    }
+
+    @Override
+    public int transaction(byte[] dataToSend, byte[] dataReceived, int size) {
+        sendBuffer.put(dataToSend); // put the send data into dataToSend
+        int resultCode = SPIJNI.spiTransaction(port, sendBuffer, receiveBuffer, this.size);
+        receiveBuffer.get(dataReceived); // pull the result data into dataRecieved
+
+        return resultCode;
+    }
+}
