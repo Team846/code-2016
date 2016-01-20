7a06d4cccba57f860efd843743dfbe9d55b32224
alanna-zhou <alanna.zhou@gmail.com>
2016-01-19 18:44:38 -0800

    Update CoreEvents and CoreRobot docs to use JavaDoc style

--- src/main/java/com/lynbrookrobotics/sixteen/CoreEvents.java
+++ src/main/java/com/lynbrookrobotics/sixteen/CoreEvents.java
@@ -13,7 +13,9 @@ import com.lynbrookrobotics.sixteen.config.RobotHardware;
 import com.lynbrookrobotics.sixteen.tasks.drivetrain.TurnByAngle;
 import edu.wpi.first.wpilibj.Joystick;
 
-//CoreEvents class creates events and maps these to handlers 
+/**
+ * CoreEvents class creates events and maps these to handlers
+ */
 public class CoreEvents {
     DriverControls controls;
     RobotHardware hardware;
@@ -27,15 +29,19 @@ public class CoreEvents {
     InGameState enabledStateEvent;
 
     // Drivetrain
-    // using lambda expression to pass updated forward & turn speeds for tank drive controller
+    /**
+     * Using lambda expression to pass updated forward & turn speeds for tank drive controller
+     */
     TankDriveController enabledDrive = TankDriveController.of(
-        () -> controls.driverStick().getAxis(Joystick.AxisType.kY),
-        () -> controls.driverWheel().getAxis(Joystick.AxisType.kX)
+            () -> controls.driverStick().getAxis(Joystick.AxisType.kY),
+            () -> controls.driverWheel().getAxis(Joystick.AxisType.kX)
     );
 
     FiniteTask auto;
 
-    // initializes hardware for events
+    /**
+     * Initializes hardware for events
+     */
     public CoreEvents(DriverControls controls, RobotHardware hardware, Drivetrain drivetrain) {
         this.controls = controls;
         this.drivetrain = drivetrain;
@@ -67,7 +73,8 @@ public class CoreEvents {
         // Drivetrain - Gyro
         disabledStateEvent.forEach(new SteadyEventHandler() {
             @Override
-            public void onStart() {}
+            public void onStart() {
+            }
 
             @Override
             public void onRunning() {
@@ -79,7 +86,8 @@ public class CoreEvents {
             }
 
             @Override
-            public void onEnd() {}
+            public void onEnd() {
+            }
         });
 
 //        enabledStateEvent.forEach(new SteadyEventHandler() {
@@ -100,7 +108,8 @@ public class CoreEvents {
 
         enabledStateEvent.forEach(new SteadyEventHandler() {
             @Override
-            public void onStart() {}
+            public void onStart() {
+            }
 
             @Override
             public void onRunning() {
@@ -109,7 +118,8 @@ public class CoreEvents {
             }
 
             @Override
-            public void onEnd() {}
+            public void onEnd() {
+            }
         });
 
         RobotConstants.dashboard().datasetGroup("drivetrain").
@@ -130,7 +140,8 @@ public class CoreEvents {
             }
 
             @Override
-            public void onRunning() {}
+            public void onRunning() {
+            }
 
             @Override
             public void onEnd() {
--- src/main/java/com/lynbrookrobotics/sixteen/CoreRobot.java
+++ src/main/java/com/lynbrookrobotics/sixteen/CoreRobot.java
@@ -15,7 +15,9 @@ import edu.wpi.first.wpilibj.Joystick;
 import java.util.Timer;
 import java.util.TimerTask;
 
-// CoreRobot class loads config and creates components
+/**
+ * CoreRobot class loads config and creates components
+ */
 public class CoreRobot {
     VariableConfiguration config = new VariableConfiguration();
     RobotHardware hardware = new RobotHardware(config);
@@ -26,7 +28,9 @@ public class CoreRobot {
 
     CoreEvents events = new CoreEvents(controls, hardware, drivetrain);
 
-    //sets up tick function with timer
+    /**
+     * Sets up tick function with timer
+     */
     public CoreRobot() {
         Timer updateTimer = new Timer("update-loop");
 
