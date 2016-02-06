bd1c82530f1d38e1cffb9693693b5adba0794e14
vikranth22446 <rama22446>
2016-02-05 20:21:16 -0800

    Add the basis for Intake

---
+++ src/main/java/com/lynbrookrobotics/sixteen/components/Intake/Intake.java
@@ -0,0 +1,13 @@
+package com.lynbrookrobotics.sixteen.components.Intake;
+
+import java.awt.*;
+
+/**
+ * Created by Vikranth on 2/5/2016.
+ */
+public class Intake extends Component {
+
+
+
+
+}
---
+++ src/main/java/com/lynbrookrobotics/sixteen/config/IntakeHardware.java
@@ -0,0 +1,26 @@
+package com.lynbrookrobotics.sixteen.config;
+
+import edu.wpi.first.wpilibj.Jaguar;
+
+import java.beans.Encoder;
+import java.time.chrono.JapaneseChronology;
+
+/**
+ * Created by Vikranth on 2/5/2016.
+ */
+public class IntakeHardware {
+    Jaguar Rightjaguar;
+    Jaguar LeftJaguar;
+    RobotHardware robotHardware;
+    DrivetrainHardware drivetrainHardware;
+    public IntakeHardware(RobotHardware robotHardware, DrivetrainHardware drivetrainHardware) {
+        this.drivetrainHardware=drivetrainHardware;
+        this.robotHardware=robotHardware;
+    }
+    public void IntakePorts() {
+
+    }
+
+
+
+}
