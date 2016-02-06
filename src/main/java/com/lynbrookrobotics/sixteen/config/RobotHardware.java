9c95d6c365caade8c2af7af60009bb291edb116b
vikranth22446 <rama22446>
2016-02-05 21:13:33 -0800

    Add documentation to Robot Hardware and Variable Cofiguration. Check spelling

--- src/main/java/com/lynbrookrobotics/sixteen/config/RobotHardware.java
+++ src/main/java/com/lynbrookrobotics/sixteen/config/RobotHardware.java
@@ -4,22 +4,37 @@ import com.lynbrookrobotics.sixteen.components.shooter.Shooter;
 import com.lynbrookrobotics.sixteen.components.arm.Arm;
 
 /**
- * Aggregation of subsystem hardware interfaces.contains instances of each subsystem hardware and their respective getters. Subsystem hardware groups should contain each individual hardware components and their respected getters.
+ * Aggregation of subsystem hardware interfaces.
+ * contains instances of each subsystem hardware and their respective getters.
+ * Subsystem hardware groups should contain each individual hardware components and their respected getters.
  */
 public class RobotHardware {
     DrivetrainHardware drivetrainHardware;
     ShooterHardware shooterHardware;
     ArmHardware armHardware;
 
+    /**
+     *  Constructs the RobotHardware
+     * @param config passes the drivetrain configuration
+     */
     public RobotHardware(VariableConfiguration config) {
         drivetrainHardware = new DrivetrainHardware(config);
     }
 
+    /**
+     * @return the drivetrain's hardware
+     */
     public DrivetrainHardware drivetrainHardware() {
         return drivetrainHardware;
     }
 
+    /**
+     * @return the shooter's hardware
+     */
     public ShooterHardware shooterHardware() { return shooterHardware; }
 
+    /**
+     * @return the arm's hardware
+     */
     public ArmHardware armHardware() { return armHardware; }
 }
