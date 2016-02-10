6262ab8f45aaff88c6fa00724fc88e0cc6a92edb
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-02-09 18:48:30 -0800

    Fix style issues in Value3D

--- src/main/java/com/lynbrookrobotics/sixteen/sensors/Value3D.java
+++ src/main/java/com/lynbrookrobotics/sixteen/sensors/Value3D.java
@@ -1,13 +1,16 @@
 package com.lynbrookrobotics.sixteen.sensors;
 
 /**
- * Represents a value with x, y and z parts
+ * Represents a value with X, Y and Z parts.
  */
 public class Value3D {
   double xValue;
   double yValue;
   double zValue;
 
+  /**
+   * Constructs a new 3D value given X, Y, and Z axes.
+   */
   public Value3D(double xValue, double yValue, double zValue) {
     this.xValue = xValue;
     this.yValue = yValue;
@@ -26,6 +29,11 @@ public class Value3D {
     return zValue;
   }
 
+  /**
+   * Adds this 3D value to another one.
+   * @param toAdd the 3D value to add
+   * @return the combined 3D value
+   */
   public Value3D plus(Value3D toAdd) {
     return new Value3D(
         this.x() + toAdd.x(),
@@ -34,20 +42,16 @@ public class Value3D {
     );
   }
 
+  /**
+   * Multiplies this 3D value by a scalar.
+   * @param scalar the value to multiply the axes by
+   * @return the scaled 3D value
+   */
   public Value3D times(double scalar) {
     return new Value3D(
         scalar * this.x(),
         scalar * this.y(),
         scalar * this.z()
     );
-  }
-
-  @Override
-  public String toString() {
-    return "GyroValue{" +
-        "xValue=" + xValue +
-        ", yValue=" + yValue +
-        ", zValue=" + zValue +
-        '}';
   }
 }
