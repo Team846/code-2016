d59715a02ac6c6b4ea5a68a95b25ed95ae8b6767
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-02-15 11:51:44 -0800

    Fix HallEffect implementation

--- src/main/java/com/lynbrookrobotics/sixteen/sensors/halleffect/HallEffect.java
+++ src/main/java/com/lynbrookrobotics/sixteen/sensors/halleffect/HallEffect.java
@@ -1,24 +1,15 @@
 package com.lynbrookrobotics.sixteen.sensors.halleffect;
 
-import com.lynbrookrobotics.funkydashboard.TimeSeriesNumeric;
-import com.lynbrookrobotics.sixteen.config.RobotConstants;
-
 import edu.wpi.first.wpilibj.Counter;
 
 public class HallEffect extends Counter {
-  double lastLast = 0;
-  double last = 0;
-
   public HallEffect(int channel) {
-    this.setUpSource(channel);
-    this.setUpDownCounterMode();
+    super(channel);
+
+//    setSamplesToAverage(); TODO: test with robot
   }
 
   public double getRPM() {
-    double ret = lastLast;
-
-    if ((last - lastLast) > 1000 && ())
-
-    return ret;
+    return 60/getPeriod();
   }
 }
