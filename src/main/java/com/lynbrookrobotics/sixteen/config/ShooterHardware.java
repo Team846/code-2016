2d541b9507abd74d7990f49ed6f2082379747174
Waffle Mans <wafflemans@users.noreply.github.com>
2016-02-14 17:09:20 -0800

    Added implementation for proximity sensors

--- src/main/java/com/lynbrookrobotics/sixteen/config/ShooterHardware.java
+++ src/main/java/com/lynbrookrobotics/sixteen/config/ShooterHardware.java
@@ -1,5 +1,6 @@
 package com.lynbrookrobotics.sixteen.config;
 
+import com.lynbrookrobotics.sixteen.sensors.proximitysensor.ProximitySensor;
 import com.lynbrookrobotics.sixteen.sensors.halleffect.HallEffect;
 
 import edu.wpi.first.wpilibj.Talon;
@@ -9,6 +10,7 @@ public class ShooterHardware {
   public final Talon backWheelMotor;
   public final HallEffect frontHallEffect;
   public final HallEffect backHallEffect;
+  public final ProximitySensor proximitySensor;
 
   /**
    * Constructs a new default ShooterHardware object given the interfaces.
@@ -16,11 +18,13 @@ public class ShooterHardware {
   public ShooterHardware(Talon frontWheel,
                          Talon backWheel,
                          HallEffect frontHall,
-                         HallEffect backHall) {
+                         HallEffect backHall,
+                         ProximitySensor proximity) {
     frontWheelMotor = frontWheel;
     backWheelMotor = backWheel;
     frontHallEffect = frontHall;
     backHallEffect = backHall;
+    proximitySensor = proximity;
   }
 
   /**
@@ -32,6 +36,7 @@ public class ShooterHardware {
       new Talon(config.shooterPorts().portFrontWheel()),
       new Talon(config.shooterPorts().portBackWheel()),
       new HallEffect(config.shooterPorts().portFrontHall()),
-      new HallEffect(config.shooterPorts().portBackHall()));
+      new HallEffect(config.shooterPorts().portBackHall()),
+        new ProximitySensor(0));
   }
 }
