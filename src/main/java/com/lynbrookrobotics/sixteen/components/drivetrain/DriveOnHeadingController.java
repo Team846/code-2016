e21c7074c7c09de6f68606f200f9dd0c0c34b530
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-02-15 17:31:25 -0800

    Update drivetrain controllers to use PID class

--- src/main/java/com/lynbrookrobotics/sixteen/components/drivetrain/DriveOnHeadingController.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/drivetrain/DriveOnHeadingController.java
@@ -1,30 +1,13 @@
 package com.lynbrookrobotics.sixteen.components.drivetrain;
 
-import com.lynbrookrobotics.funkydashboard.TimeSeriesNumeric;
-import com.lynbrookrobotics.sixteen.config.RobotConstants;
 import com.lynbrookrobotics.sixteen.config.RobotHardware;
+import com.lynbrookrobotics.sixteen.control.pid.PID;
 import com.lynbrookrobotics.sixteen.sensors.digitalgyro.DigitalGyro;
 
 import java.util.function.Supplier;
 
 public class DriveOnHeadingController extends TankDriveController {
-  static double lastError = 0;
-  static double integral = 0;
-  static double forward = 0;
-
-  static {
-    RobotConstants.dashboard().datasetGroup("drivetrain-controllers")
-        .addDataset(new TimeSeriesNumeric<>("PID error", () -> lastError));
-
-    RobotConstants.dashboard().datasetGroup("drivetrain-controllers")
-        .addDataset(new TimeSeriesNumeric<>("PID integral", () -> integral));
-
-    RobotConstants.dashboard().datasetGroup("drivetrain-controllers")
-        .addDataset(new TimeSeriesNumeric<>("Forward speed", () -> forward));
-  }
-
-  private static final double iMemory = 0.4/* IIR_DECAY(5.0) */;
-  private double runningIntegral = 0.0;
+  private PID angleControl;
 
   RobotHardware hardware;
   DigitalGyro gyro;
@@ -42,37 +25,20 @@ public class DriveOnHeadingController extends TankDriveController {
     this.gyro = hardware.drivetrainHardware().mainGyro();
     this.targetAngle = angle;
     this.forwardSpeed = speed;
-  }
 
-  private double difference() {
-    double ret = targetAngle - gyro.currentPosition().valueZ();
-    lastError = ret;
-    return ret;
-  }
-
-  private double updateIntegral(double value) {
-    runningIntegral = (runningIntegral * iMemory)
-        + ((1 - iMemory) * value * RobotConstants.TICK_PERIOD);
-    integral = runningIntegral;
-
-    return runningIntegral;
-  }
-
-  private double piOutput() {
-    double proportionalOut = difference() * (1D / (4 * 90));
-    double integralOut = updateIntegral(difference()) * (1.5D / (90));
-
-    return proportionalOut + integralOut;
+    this.angleControl = new PID(
+        () -> hardware.drivetrainHardware().mainGyro().currentPosition().valueZ(),
+        angle
+    ).withP(1D / (4 * 90)).withI(1.5D / (90), 0.4);
   }
 
   @Override
   public double forwardSpeed() {
-    forward = forwardSpeed.get();
-    return forward;
+    return forwardSpeed.get();
   }
 
   @Override
   public double turnSpeed() {
-    return piOutput();
+    return angleControl.get();
   }
 }
--- src/main/java/com/lynbrookrobotics/sixteen/components/drivetrain/TurnByAngleController.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/drivetrain/TurnByAngleController.java
@@ -1,15 +1,13 @@
 package com.lynbrookrobotics.sixteen.components.drivetrain;
 
 import com.lynbrookrobotics.sixteen.config.RobotHardware;
-import com.lynbrookrobotics.sixteen.sensors.digitalgyro.DigitalGyro;
+import com.lynbrookrobotics.sixteen.control.pid.PID;
 
 /**
  * A controller that turns a specific number of degrees.
  */
 public class TurnByAngleController extends TankDriveController {
-  RobotHardware hardware;
-  DigitalGyro gyro;
-  double targetAngle;
+  private PID angleControl;
 
   /**
    * Constructs a new TurnByAngleController.
@@ -17,13 +15,10 @@ public class TurnByAngleController extends TankDriveController {
    * @param hardware the robot hardware to use
    */
   public TurnByAngleController(double angle, RobotHardware hardware) {
-    this.hardware = hardware;
-    this.gyro = hardware.drivetrainHardware().mainGyro();
-    this.targetAngle = gyro.currentPosition().valueZ() + angle;
-  }
-
-  public double difference() {
-    return targetAngle - gyro.currentPosition().valueZ();
+    this.angleControl = new PID(
+        () -> hardware.drivetrainHardware().mainGyro().currentPosition().valueZ(),
+        hardware.drivetrainHardware().mainGyro().currentPosition().valueZ() + angle
+    ).withP(1D / (4 * 90)).withI(1.5D / (90), 0.4);
   }
 
   @Override
@@ -33,6 +28,6 @@ public class TurnByAngleController extends TankDriveController {
 
   @Override
   public double turnSpeed() {
-    return difference() * (1D / 360);
+    return angleControl.get();
   }
 }
