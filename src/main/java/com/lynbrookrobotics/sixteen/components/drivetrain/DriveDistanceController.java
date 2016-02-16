302a4c5ba71a0700d215110f833baadbb4698105
Philip Axelrod <philip.axelrod.web@gmail.com>
2016-02-16 14:47:41 -0800

    Fixed style issues

--- src/main/java/com/lynbrookrobotics/sixteen/components/drivetrain/DriveDistanceController.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/drivetrain/DriveDistanceController.java
@@ -7,36 +7,43 @@ import com.lynbrookrobotics.sixteen.sensors.digitalgyro.DigitalGyro;
 import java.util.function.Supplier;
 
 /**
- * A class that drives to an absolute position. It returns
+ * A controller that drives to an absolute position.
  */
 public class DriveDistanceController extends DrivetrainController {
-    RobotHardware hardware;
+  RobotHardware hardware;
 
-    private PID leftSpeedControl;
-    private PID rightSpeedControl;
+  private PID leftSpeedControl;
+  private PID rightSpeedControl;
 
-    public DriveDistanceController(double angle, RobotHardware hardware, double targetPosition) {
-        this.hardware = hardware;
+  /**
+   * Controller that drives to an absolute position.
+   * @param hardware The robot hardware
+   * @param leftTargetPosition Target position in terms of degrees that left encoder is turned
+   * @param rightTargetPosition Target position in terms of degrees that left encoder is turned
+   */
+  public DriveDistanceController( RobotHardware hardware, double leftTargetPosition,
+                                  double rightTargetPosition) {
+    this.hardware = hardware;
 
-        this.leftSpeedControl = new PID(
-                () -> hardware.drivetrainHardware.getLeftEncoder().getPosition(),
-                targetPosition)
+    this.leftSpeedControl = new PID(
+        () -> hardware.drivetrainHardware.getLeftEncoder().getPosition(),
+        leftTargetPosition)
         .withP(1D / (4 * 90)).withI(1.5D / (90), 0.4);
 
-        this.rightSpeedControl = new PID(
-                () -> hardware.drivetrainHardware.getRightEncoder().getPosition(),
-                targetPosition)
+    this.rightSpeedControl = new PID(
+        () -> hardware.drivetrainHardware.getRightEncoder().getPosition(),
+        rightTargetPosition)
         .withP(1D / (4 * 90)).withI(1.5D / (90), 0.4);
-    }
+  }
 
-    @Override
-    public double leftSpeed() {
-        return leftSpeedControl.get();
-    }
+  @Override
+  public double leftSpeed() {
+    return leftSpeedControl.get();
+  }
 
-    @Override
-    public double rightSpeed() {
-        return rightSpeedControl.get();
-    }
+  @Override
+  public double rightSpeed() {
+    return rightSpeedControl.get();
+  }
 
 }
