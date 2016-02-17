2471282450948cb4b814d32c68ef04f2417ddd1d
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-02-16 16:08:01 -0800

    Add JavaDoc to PID and move to sbt-frc release

--- src/main/java/com/lynbrookrobotics/sixteen/control/pid/PID.java
+++ src/main/java/com/lynbrookrobotics/sixteen/control/pid/PID.java
@@ -18,17 +18,31 @@ public class PID {
 
   private double runningIntegral;
 
+  /**
+   * Constructs a new PID controller
+   * @param input the function producing input data
+   * @param target the target value for the input
+   */
   public PID(Supplier<Double> input, double target) {
     this.input = input;
     this.target = target;
   }
 
+  /**
+   * Adds a proportional component to the controller
+   * @param gain the gain to apply to error
+   */
   public PID withP(double gain) {
     pGain = gain;
 
     return this;
   }
 
+  /**
+   * Adds an integration component to the controller
+   * @param gain the gain to apply to integrated error
+   * @param memory the memory of the integration
+   */
   public PID withI(double gain, double memory) {
     iGain = gain;
     iMemory = memory;
@@ -36,16 +50,26 @@ public class PID {
     return this;
   }
 
+  /**
+   * Adds a derivative component to the controller
+   * @param gain the gain to apply to the derivative of error
+   */
   public PID withD(double gain) {
     dGain = gain;
 
     return this;
   }
 
+  /**
+   * Gets the difference to the target value
+   */
   public double difference() {
     return target - input.get();
   }
 
+  /**
+   * Gets the output of the PID controller
+   */
   public double get() {
     double in = input.get();
     double error = target - in;
