3cc109a3921a56df60965348eb4c1bf794f38efc
alanna-zhou <alanna.zhou@gmail.com>
2016-02-16 20:42:54 -0800

    Add IntakeArm

--- src/main/java/com/lynbrookrobotics/sixteen/components/intake/arm/IntakeArmController.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/intake/arm/IntakeArmController.java
@@ -1,5 +1,16 @@
 package com.lynbrookrobotics.sixteen.components.intake.arm;
 
+import java.util.function.Supplier;
+
 public abstract class IntakeArmController {
-  public abstract void angle();
+  public static IntakeArmController of(Supplier<Double> speed) {
+    return new IntakeArmController() {
+      @Override
+      public double armSpeed() {
+        return speed.get();
+      }
+    };
+  }
+
+  public abstract double armSpeed();
 }
