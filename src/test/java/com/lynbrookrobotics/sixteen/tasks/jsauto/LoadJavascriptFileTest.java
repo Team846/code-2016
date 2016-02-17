3328f6ecb0a2bb5353298b274ef21fb93fb9dc72
Aubhro <aubrosengupta@gmail.com>
2016-02-16 18:40:45 -0800

    Changed drivetrain
    updated

--- src/test/java/com/lynbrookrobotics/sixteen/tasks/jsauto/LoadJavascriptFileTest.java
+++ src/test/java/com/lynbrookrobotics/sixteen/tasks/jsauto/LoadJavascriptFileTest.java
@@ -1,6 +1,7 @@
 package com.lynbrookrobotics.sixteen.tasks.jsauto;
 
 import com.lynbrookrobotics.sixteen.components.drivetrain.Drivetrain;
+import com.lynbrookrobotics.sixteen.components.shooter.spinners.ShooterSpinners;
 import com.lynbrookrobotics.sixteen.config.RobotHardware;
 import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
 
@@ -11,19 +12,20 @@ import org.mozilla.javascript.EvaluatorException;
 
 public class LoadJavascriptFileTest {
 
-  private static String correctCode = "new TurnByAngle(137.3, robotHardware, driveTrain);";
-  private static String incorrectCode = "new TurnByAngle(137.3, driveTrain, robotHardware);";
+  private static String correctCode = "new TurnByAngle(137.3, robotHardware, drivetrain);";
+  private static String incorrectCode = "new TurnByAngle(137.3, drivetrain, robotHardware);";
 
   @Rule
   public ExpectedException thrown = ExpectedException.none();
 
-  RobotHardware hardware = new RobotHardware(null, null, null);
-  Drivetrain drivetrain = new Drivetrain(hardware, null);
+  RobotHardware hardware = new RobotHardware(null, null, null, null);
+  Drivetrain drivetrain = new Drivetrain(hardware);
+  ShooterSpinners spinners = new ShooterSpinners(hardware);
 
   @Test
   public void testLoadStringCorrect() {
     LoadJavascriptFile loader = new LoadJavascriptFile();
-    String result = loader.loadString(correctCode, null , null).toString();
+    String result = loader.loadString(correctCode, hardware, drivetrain, spinners).toString();
     assert result.startsWith("com.lynbrookrobotics.sixteen.tasks.drivetrain.TurnByAngle");
 
 
@@ -32,7 +34,7 @@ public class LoadJavascriptFileTest {
   @Test(expected = EvaluatorException.class)
   public void testLoadStringIncorrect() {
     LoadJavascriptFile loader = new LoadJavascriptFile();
-    String result = loader.loadString(incorrectCode, hardware , drivetrain).toString();
+    String result = loader.loadString(incorrectCode, hardware, drivetrain, spinners).toString();
     assert result.startsWith("com.lynbrookrobotics.sixteen.tasks.drivetrain.TurnByAngle");
   }
 }
