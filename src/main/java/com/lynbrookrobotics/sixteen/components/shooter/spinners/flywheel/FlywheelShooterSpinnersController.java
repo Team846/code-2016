9375bd0e4c8801f3cc7a7eb66930c4ff2965d0ad
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-02-17 13:20:37 -0800

    Simplify controllers for shooter

--- src/main/java/com/lynbrookrobotics/sixteen/components/shooter/spinners/flywheel/FlywheelShooterSpinnersController.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/shooter/spinners/flywheel/FlywheelShooterSpinnersController.java
@@ -1,9 +1,20 @@
 package com.lynbrookrobotics.sixteen.components.shooter.spinners.flywheel;
 
+import java.util.function.Supplier;
+
 /**
  * The controller for the shooter component.
  */
 public abstract class FlywheelShooterSpinnersController {
+  public static FlywheelShooterSpinnersController of(Supplier<Double> speed) {
+    return new FlywheelShooterSpinnersController() {
+      @Override
+      public double flywheelSpeed() {
+        return speed.get();
+      }
+    };
+  }
+
   /**
    * Gets the current speed of the flywheel as a normalized value.
    */
--- src/main/java/com/lynbrookrobotics/sixteen/components/shooter/spinners/secondary/SecondaryShooterSpinnersController.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/shooter/spinners/secondary/SecondaryShooterSpinnersController.java
@@ -1,6 +1,16 @@
 package com.lynbrookrobotics.sixteen.components.shooter.spinners.secondary;
 
+import java.util.function.Supplier;
+
 public abstract class SecondaryShooterSpinnersController {
+  public static SecondaryShooterSpinnersController of(Supplier<Double> speed) {
+    return new SecondaryShooterSpinnersController() {
+      @Override
+      public double secondarySpeed() {
+        return speed.get();
+      }
+    };
+  }
 
   /**
    *  Gets velocity of secondary wheel as normalized value.
