224aa0c280c92aa87489c4063d9f2b869cec88a4
Aubhro <aubrosengupta@gmail.com>
2016-02-17 20:07:49 -0800

    Added boolean method
    Adds method that returns if arm is stowed

--- src/main/java/com/lynbrookrobotics/sixteen/components/shooter/arm/ShooterArm.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/shooter/arm/ShooterArm.java
@@ -7,17 +7,28 @@ import edu.wpi.first.wpilibj.CANTalon;
 
 public class ShooterArm extends Component<ShooterArmController> {
   private final CANTalon armMotor;
-
+  private RobotHardware robotHardware;
   /**
    * Constructs a shooter arm component.
    */
+<<<<<<< 59cc8e2ab462927f5f77a7659878967d142830d7
   public ShooterArm(RobotHardware hardware) {
     super(ShooterArmController.of(() -> 0.0));
     this.armMotor = hardware.shooterArmHardware.armMotor;
+=======
+  public ShooterArm(RobotHardware robotHardware, ShooterArmController defaultController) {
+    super(defaultController);
+    this.robotHardware = robotHardware;
+    this.armMotor = robotHardware.shooterArmHardware.armMotor;
+>>>>>>> Added boolean method
   }
 
   @Override
   protected void setOutputs(ShooterArmController controller) {
     armMotor.set(controller.armMotorSpeed());
+  }
+
+  public boolean IsStowed() {
+    return robotHardware.shooterArmHardware.encoder.getAngle() == 0;
   }
 }
--- src/main/java/com/lynbrookrobotics/sixteen/components/shooter/arm/ShooterArmPositionController.java
+++ src/main/java/com/lynbrookrobotics/sixteen/components/shooter/arm/ShooterArmPositionController.java
@@ -1,30 +1,26 @@
 package com.lynbrookrobotics.sixteen.components.shooter.arm;
 
 import com.lynbrookrobotics.sixteen.config.RobotHardware;
+import com.lynbrookrobotics.sixteen.config.constants.IntakeArmConstants;
 import com.lynbrookrobotics.sixteen.config.constants.ShooterArmConstants;
 import com.lynbrookrobotics.sixteen.control.pid.PID;
 
 public class ShooterArmPositionController extends ShooterArmController {
   private PID pid;
+  private RobotHardware robotHardware;
+  double currentPosition;
 
   /**
    * Constructor for the ShooterArmPositionController.
    * @param targetPotPosition the target pot position
-   * @param hardware          the robot hardware
+   * @param robotHardware          the robot hardware
    */
-<<<<<<< 3fdb81296cd62398d582485b758e884e7ce98839
-  public ShooterArmPositionController(double targetPotPosition, RobotHardware hardware) {
-    pid = new PID(hardware.shooterArmHardware.pot::getAngle, targetPotPosition)
-        .withP(ShooterArmConstants.P_GAIN)
-        .withI(ShooterArmConstants.I_GAIN,
-            ShooterArmConstants.I_MEMORY);
-=======
   public ShooterArmPositionController(int targetPotPosition, RobotHardware robotHardware) {
     this.robotHardware = robotHardware;
     if ( armMotorSpeed() < 0 && isIntakeArmStowed() ) {
       System.out.println("Intake arm is stowed, cannot move shooter arm there");
     } else {
-      pid = new PID(() -> (double) currentPosition, (double) targetPotPosition)
+      pid = new PID(() -> currentPosition, (double) targetPotPosition)
           .withP(ShooterArmConstants.P_GAIN)
           .withI(ShooterArmConstants.I_GAIN,
               ShooterArmConstants.I_MEMORY);
@@ -37,8 +33,7 @@ public class ShooterArmPositionController extends ShooterArmController {
    * @return intake stowed or not.
    */
   public boolean isIntakeArmStowed() {
-    return Math.abs(robotHardware.intakeArmHardware.encoder.getAngle() - IntakeArmConstants.STOWED_SETPOINT) < IntakeArmConstants.ARM_ERROR;
->>>>>>> fixed merge issues
+    return Math.abs(robotHardware.intakeArmHardware.pot.getAngle() - IntakeArmConstants.STOWED_SETPOINT) < IntakeArmConstants.ARM_ERROR;
   }
 
   /**
