b942dd06abec025a46e0f8526e90e5768a99b719
Timothy Yang <tyang108@student.fuhsd.org>
2016-02-18 13:12:27 -0800

    Implemented moving shooter arm into Shoot task

--- src/main/java/com/lynbrookrobotics/sixteen/tasks/shooter/ShooterTasks.java
+++ src/main/java/com/lynbrookrobotics/sixteen/tasks/shooter/ShooterTasks.java
@@ -1,10 +1,12 @@
 package com.lynbrookrobotics.sixteen.tasks.shooter;
 
 import com.lynbrookrobotics.potassium.tasks.FiniteTask;
+import com.lynbrookrobotics.sixteen.components.shooter.arm.ShooterArm;
 import com.lynbrookrobotics.sixteen.components.shooter.spinners.flywheel.ShooterFlywheel;
 import com.lynbrookrobotics.sixteen.components.shooter.spinners.secondary.ShooterSecondary;
 import com.lynbrookrobotics.sixteen.config.RobotHardware;
 import com.lynbrookrobotics.sixteen.tasks.FixedTime;
+import com.lynbrookrobotics.sixteen.tasks.shooter.arm.MoveShooterArmToAngle;
 import com.lynbrookrobotics.sixteen.tasks.shooter.spinners.flywheel.SpinFlywheelAtRPM;
 import com.lynbrookrobotics.sixteen.tasks.shooter.spinners.flywheel.SpinFlywheelToRPM;
 import com.lynbrookrobotics.sixteen.tasks.shooter.spinners.secondary.SpinSecondary;
@@ -20,18 +22,20 @@ public class ShooterTasks {
    * @param hardware Robot Hardware
    * @param targetRPM RPM of the Flywheel
    * @param speed Speed of the secondary wheel
+   * @param angle Angle of the shooter arm
    * @return FiniteTask for shooting
    */
   public static FiniteTask shoot(ShooterFlywheel shooterFlywheel,
                                  ShooterSecondary shooterSecondary,
+                                 ShooterArm shooterArm,
                                  RobotHardware hardware,
                                  double targetRPM,
-                                 double speed) {
+                                 double speed,
+                                 int angle) {
     SpinFlywheelAtRPM flywheelTask
         = new SpinFlywheelAtRPM(targetRPM, shooterFlywheel, hardware);
-    return new SpinFlywheelToRPM(targetRPM,
-                                 shooterFlywheel,
-                                 hardware)
+    return new MoveShooterArmToAngle(angle, hardware, shooterArm)
+        .and(new SpinFlywheelToRPM(targetRPM, shooterFlywheel, hardware))
         .then(new SpinSecondaryNoBall(speed,
                                       shooterSecondary,
                                       hardware)
