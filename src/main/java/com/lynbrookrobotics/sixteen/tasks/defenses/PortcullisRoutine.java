2661bf351e9e79845d109a9f0d1484495aa471ef
Vikranth Srivatsa <rama22446@gamil.com>
2016-02-18 17:35:25 -0800

    Fix merge Issues

--- src/main/java/com/lynbrookrobotics/sixteen/tasks/defenses/PortcullisRoutine.java
+++ src/main/java/com/lynbrookrobotics/sixteen/tasks/defenses/PortcullisRoutine.java
@@ -5,23 +5,28 @@ import com.lynbrookrobotics.sixteen.components.drivetrain.Drivetrain;
 import com.lynbrookrobotics.sixteen.components.intake.arm.IntakeArm;
 import com.lynbrookrobotics.sixteen.config.RobotHardware;
 import com.lynbrookrobotics.sixteen.config.constants.IntakeArmConstants;
-import com.lynbrookrobotics.sixteen.tasks.drivetrain.ContinuousDrive;
+import com.lynbrookrobotics.sixteen.tasks.drivetrain.DriveRelative;
 import com.lynbrookrobotics.sixteen.tasks.intake.arm.MoveIntakeArmToAngle;
 
 
 public class PortcullisRoutine {
 
-  public static FiniteTask crossPorticullis(IntakeArm intakeArm, Drivetrain drivetrain,
-                                            RobotHardware robotHardware) {
-
-    return new MoveIntakeArmToAngle
-        (IntakeArmConstants.LOW_POSITON_PORTCULLIS, intakeArm, robotHardware)
-        .then(new MoveIntakeArmToAngle
-            (IntakeArmConstants.HIGH_POSITON_PORTCULLIS, intakeArm, robotHardware))
-        .andUntilDone(new ContinuousDrive(
-            () -> IntakeArmConstants.FORWARD_DISTANCE_PORTCULLIS,
-            () -> 0.0,//Assume 0.0 because driving straight
-            robotHardware,
+  public static FiniteTask crossPortcullis(IntakeArm intakeArm, Drivetrain drivetrain,
+                                           RobotHardware robotHardware) {
+    return new MoveIntakeArmToAngle(
+        IntakeArmConstants.LOW_POSITION_PORTCULLIS,
+        intakeArm,
+        robotHardware
+    ).then(
+        new MoveIntakeArmToAngle(
+            IntakeArmConstants.HIGH_POSITION_PORTCULLIS,
+            intakeArm,
+            robotHardware
+        )
+    ).then(
+        new DriveRelative(robotHardware,
+            IntakeArmConstants.DRIVING_DISTANCE_PORTCULLIS,
+            IntakeArmConstants.DRIVING_DISTANCE_PORTCULLIS,
             drivetrain
         ));
   }
