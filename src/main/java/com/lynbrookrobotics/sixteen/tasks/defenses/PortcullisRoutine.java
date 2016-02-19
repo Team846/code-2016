3de08baf43e77a68d0204df7695dfe661e642fd9
Vikranth Srivatsa <rama22446@gamil.com>
2016-02-18 17:26:48 -0800

    Fix stylistic issues

--- src/main/java/com/lynbrookrobotics/sixteen/tasks/defenses/PortcullisRoutine.java
+++ src/main/java/com/lynbrookrobotics/sixteen/tasks/defenses/PortcullisRoutine.java
@@ -8,9 +8,17 @@ import com.lynbrookrobotics.sixteen.config.constants.IntakeArmConstants;
 import com.lynbrookrobotics.sixteen.tasks.drivetrain.DriveRelative;
 import com.lynbrookrobotics.sixteen.tasks.intake.arm.MoveIntakeArmToAngle;
 
-
+/**
+ * The Routine used when passing through defense portcullis.
+ */
 public class PortcullisRoutine {
-
+  /**
+   * The Routine used when passing through defense portcullis.
+   * @param intakeArm The intake arm used to move the portcullis to a certain position.
+   * @param drivetrain The drive train used to move through portcullis after lifting.
+   * @param robotHardware The robot hardware used to access the ports.
+   * @return A finite task with the portcullis routine.
+   */
   public static FiniteTask crossPortcullis(IntakeArm intakeArm, Drivetrain drivetrain,
                                            RobotHardware robotHardware) {
     return new MoveIntakeArmToAngle(
