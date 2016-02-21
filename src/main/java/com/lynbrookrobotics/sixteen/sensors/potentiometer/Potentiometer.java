7ee640ee365bbd2f64fd28102dfe5bae053486f6
Philip Axelrod <philip.axelrod.web@gmail.com>
2016-02-20 16:38:48 -0800

    Made pot class work with guitar pot

--- src/main/java/com/lynbrookrobotics/sixteen/sensors/potentiometer/Potentiometer.java
+++ src/main/java/com/lynbrookrobotics/sixteen/sensors/potentiometer/Potentiometer.java
@@ -3,21 +3,21 @@ package com.lynbrookrobotics.sixteen.sensors.potentiometer;
 import edu.wpi.first.wpilibj.AnalogInput;
 
 /**
- * Class for a ten turn Bournes potentiometer.
+ * Class for the guitar potentiometer  with part #:987 -1325
  */
 public class Potentiometer {
   AnalogInput input;
   double conversionFactor = 726;
-  double offset = 2.658691068;
+  double positionOffset;
+  double voltageOffset = 13.122;
 
   /**
    * Class for a ten turn potentiometer.
    * @param channel The input channel of the pot on the roborio's analog input
-   * @param offset The value by which the potentiometer's values are offset by
    */
-  public Potentiometer(int channel, double offset) {
+  public Potentiometer(int channel, double positionOffset) {
     this.input = new AnalogInput(channel);
-    this.offset = offset;
+    this.positionOffset = positionOffset;
   }
 
   /**
@@ -25,7 +25,8 @@ public class Potentiometer {
    * @return angle that the pot has turned in degrees
    */
   public double getAngle() {
-    return conversionFactor * input.getAverageVoltage() - offset;
+    return conversionFactor * (input.getAverageVoltage() - voltageOffset) -
+            positionOffset;
   }
 
 }
