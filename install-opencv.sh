1ade755f6039c31c281ed6ca00d83f9fd3f3c84b
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-02-28 14:39:42 -0800

    Add step at beginning at Travis build to instal OpenCV

---
+++ install-opencv.sh
@@ -0,0 +1,13 @@
+git clone https://github.com/Itseez/opencv.git
+cd opencv
+git checkout tags/3.1.0
+cd ..
+mkdir opencv-build
+cd opencv-build
+cmake ../opencv
+make -j4
+sudo make -j4 install
+cd ..
+mkdir vision/lib
+cp /usr/local/share/OpenCV/java/opencv-310.jar vision/lib/opencv-310.jar
+cp /usr/local/share/OpenCV/java/libopencv_java310.so vision/lib/libopencv_java310.so
