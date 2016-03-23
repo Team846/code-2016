f1ed8e0bd8fb103fe195ec198d7ec569b9247adf
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-03-23 16:37:09 -0700

    Enable caching for compiled OpenCV

--- install-opencv.sh
+++ install-opencv.sh
@@ -1,12 +1,15 @@
-git clone https://github.com/Itseez/opencv.git
-cd opencv
-git checkout tags/3.1.0
-cd ..
-mkdir opencv-build
-cd opencv-build
-cmake ../opencv
-make -j4
-cd ..
+if [ ! -d "opencv-build-3.1.0" ]; then
+  git clone https://github.com/Itseez/opencv.git
+  cd opencv
+  git checkout tags/3.1.0
+  cd ..
+  mkdir opencv-build-3.1.0
+  cd opencv-build-3.1.0
+  cmake ../opencv
+  make -j2
+  cd ..
+fi
+
 mkdir vision/lib
-cp opencv-build/bin/opencv-310.jar vision/lib/opencv-310.jar
-cp opencv-build/lib/libopencv_java310.so vision/lib/libopencv_java310.so
+cp opencv-build-3.1.0/bin/opencv-310.jar vision/lib/opencv-310.jar
+cp opencv-build-3.1.0/lib/libopencv_java310.so vision/lib/libopencv_java310.so
