b2a2ec2cd421460f46815a98116c12893586bc09
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-03-26 17:26:51 -0700

    Add in vision constants and convert degrees and radians properly

--- src/main/java/com/lynbrookrobotics/sixteen/config/constants/VisionConstants.java
+++ src/main/java/com/lynbrookrobotics/sixteen/config/constants/VisionConstants.java
@@ -1,15 +1,15 @@
 package com.lynbrookrobotics.sixteen.config.constants;
 
 public class VisionConstants {
-  public static final int IMAGE_HEIGHT = 0;
-  public static final int IMAGE_WIDTH = 0;
+  public static final int IMAGE_HEIGHT = 720;
+  public static final int IMAGE_WIDTH = 1280;
 
-  public static final double IMAGE_VERTICAL_FOV = 0;
-  public static final double IMAGE_HORIZONTAL_FOV = 0;
+  public static final double IMAGE_VERTICAL_FOV = 33.6;
+  public static final double IMAGE_HORIZONTAL_FOV = 59.7;
 
   public static final double CAMERA_TILT = 0;
 
-  public static final double TOWER_HEIGHT = 0;
+  public static final double TOWER_HEIGHT = 53.251 / 12D;
   public static final double CAMERA_HEIGHT = 0;
   public static final double CAMERA_TOWER_HEIGHT = TOWER_HEIGHT - CAMERA_HEIGHT;
 
