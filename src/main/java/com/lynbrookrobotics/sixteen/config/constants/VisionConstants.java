9f759953322c3bf7fc8ac0dd82cb3611ba10a67a
Sang Hak (Andy) Chun <sanghak@andychun.info>
2016-09-17 15:41:11 -0700

    Adjust vision constants

--- src/main/java/com/lynbrookrobotics/sixteen/config/constants/VisionConstants.java
+++ src/main/java/com/lynbrookrobotics/sixteen/config/constants/VisionConstants.java
@@ -5,15 +5,16 @@ public class VisionConstants {
   public static final int IMAGE_HEIGHT = 240;
   public static final int IMAGE_WIDTH = 320;
 
-  public static final double IMAGE_VERTICAL_FOV = 33.6; // TODO find out for logitech cam
-  public static final double IMAGE_HORIZONTAL_FOV = 60;
+  public static final double IMAGE_VERTICAL_FOV = 70.0D;
+//  public static final double IMAGE_HORIZONTAL_FOV = 59.7D;
+  public static final double IMAGE_HORIZONTAL_FOV = 70.0D;
 
-  public static final double CAMERA_TILT = 58;
+  public static final double CAMERA_TILT = 20D; // degrees camera is offset from vertical
 
-  public static final double TOWER_HEIGHT = 53.251 / 12D;
-  public static final double CAMERA_HEIGHT = 9 / 12D;
-  public static final double CAMERA_TOWER_HEIGHT = TOWER_HEIGHT - CAMERA_HEIGHT;
+  public static final double TOWER_HEIGHT = 53.251D / 12D; // tower height from ground
+  public static final double CAMERA_HEIGHT = 10.5D / 12D; // camera height from ground
+  public static final double CAMERA_TOWER_HEIGHT = TOWER_HEIGHT - CAMERA_HEIGHT; // tower height from camera level
 
-  public static final double CAMERA_TO_MIDDLE_HORIZONTAL = -11D / 12;
-  public static final double MIDDLE_TO_CAMERA_FORWARD = 10 / 12D;
+  public static final double CAMERA_TO_MIDDLE_HORIZONTAL = 11D / 12D; // distance camera is from center of robot (side axis)
+  public static final double MIDDLE_TO_CAMERA_FORWARD = 10D / 12D;
 }
