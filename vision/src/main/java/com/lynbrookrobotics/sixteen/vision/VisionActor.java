34a2b45326c8f9da6c035fdb2a02f62a0f17eece
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-09-23 16:32:20 -0700

    Remove v4l2 command line use because of python launcher script

--- vision/src/main/java/com/lynbrookrobotics/sixteen/vision/VisionActor.java
+++ vision/src/main/java/com/lynbrookrobotics/sixteen/vision/VisionActor.java
@@ -36,27 +36,10 @@ public class VisionActor extends UntypedActor {
   public VisionActor(InetSocketAddress roboRIO) {
     this.roboRIO = roboRIO;
 
-    try {
-      new ProcessBuilder("/usr/bin/v4l2-ctl", "-c", "exposure_auto=1", "-c", "exposure_absolute=20").start().waitFor();
-    } catch (IOException e) {
-      e.printStackTrace();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-
     camera = new VideoCapture(0);
 
     camera.set(Videoio.CAP_PROP_FRAME_WIDTH, 320);
     camera.set(Videoio.CAP_PROP_FRAME_HEIGHT, 200);
-
-    try {
-      Thread.sleep(5000);
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    }
-
-    camera.release();
-    camera = new VideoCapture(0);
 
     // request creation of a SimpleSender
     final ActorRef mgr = Udp.get(getContext().system()).getManager();
