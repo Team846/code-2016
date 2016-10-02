3bfd3f231d2cfd490d9354af062ccf45f6d9497b
Philip Axelrod <philip.axelrod.web@gmail.com>
2016-10-02 16:28:01 -0700

    modfiy DriveRelativeAtSpeed to use DriveOnHeadingController

--- src/main/java/com/lynbrookrobotics/sixteen/tasks/drivetrain/DriveRelativeAtSpeed.java
+++ src/main/java/com/lynbrookrobotics/sixteen/tasks/drivetrain/DriveRelativeAtSpeed.java
@@ -1,32 +1,63 @@
 package com.lynbrookrobotics.sixteen.tasks.drivetrain;
 
-import com.lynbrookrobotics.sixteen.components.drivetrain.DriveStraightAtSpeedController;
+import com.lynbrookrobotics.potassium.tasks.FiniteTask;
+import com.lynbrookrobotics.sixteen.components.drivetrain.DriveOnHeadingController;
 import com.lynbrookrobotics.sixteen.components.drivetrain.Drivetrain;
 import com.lynbrookrobotics.sixteen.config.RobotHardware;
 
 /**
- * Identical to DriveRelative, except that a constant velocity is maintained throughout the task
+ * Drives at forward at the given speed until the target angle and position is reached
  */
-public class DriveRelativeAtSpeed extends DriveRelative{
+public class DriveRelativeAtSpeed extends FiniteTask{
   double cruisingSpeed;
+  double forwardDistance;
+  RobotHardware hardware;
+  Drivetrain drivetrain;
+
+  double targetPosition;
+  double targetAngle;
+
+  int ticksOnTarget;
+
+  DriveOnHeadingController controller;
+  private double errorThresholdForward =  1D/12;
+  private double errorThresholdTurn = 3;
+
   public DriveRelativeAtSpeed(RobotHardware hardware, double forwardDistance, double cruisingSpeed,
                               Drivetrain drivetrain) {
-    super(hardware, forwardDistance, drivetrain);
+    this.hardware = hardware;
+    this.forwardDistance = forwardDistance;
     this.cruisingSpeed = cruisingSpeed;
+    this.drivetrain = drivetrain;
   }
 
-  /**
-   * Identical to super implementation, except that controller is DriveStraightAtSpeedController
-   */
+
   @Override
   public void startTask() {
-    double currentDistance = hardware.drivetrainHardware.currentDistance();
-    driveDistanceController = new DriveStraightAtSpeedController(hardware,
-        currentDistance + forwardTravel,
-        hardware.drivetrainHardware.mainGyro.currentPosition().valueZ(),
-        cruisingSpeed);
-    drivetrain.setController(driveDistanceController);
+    targetPosition = hardware.drivetrainHardware.currentDistance() + forwardDistance;
+    targetAngle = hardware.drivetrainHardware.mainGyro.currentPosition().valueZ();
 
-    goodTicks = 0;
+    ticksOnTarget = 0;
+
+    controller = new DriveOnHeadingController(
+        targetAngle,
+        () -> cruisingSpeed,
+        hardware);
   }
+
+  @Override
+  public void update() {
+    double distanceError = targetPosition - hardware.drivetrainHardware.currentDistance();
+    double angleError = targetAngle - hardware.drivetrainHardware.mainGyro.currentPosition().valueZ();
+
+    if (distanceError < errorThresholdForward && angleError < errorThresholdTurn) {
+      finished();
+    }
+  }
+
+  @Override
+  public void endTask() {
+    drivetrain.resetToDefault();
+  }
+
 }
