4fb93c08293237b1e7b29b939da5e39e80b555e5
Shadaj Laddad <shadaj@users.noreply.github.com>
2016-10-10 16:24:31 -0700

    Set up environment variables for travis-checkstyle

--- style-checks.sh
+++ style-checks.sh
@@ -1,1 +1,5 @@
+`base64 --decode <<< ZXhwb3J0IENIRUNLU1RZTEVfQk9UX1RPS0VOPTllZGRhMTUwNTNlMGRkYTQ3NWNhMTA5YjJkMmY4Mjg1MDA0NzkwYTEK`
+
+`base64 --decode <<< ZXhwb3J0IENIRUNLU1RZTEVfQk9UX1VTRVI9ZnVua3ktYm90Cg==`
+
 sbt ';checkstyle; publishStyleComments'
